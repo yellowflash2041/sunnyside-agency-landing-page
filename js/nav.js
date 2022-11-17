9463a029ce503a2d42f317523cb08147ac72665e
yellowflash2041 <yellowflash2041@gmail.com>
2022-11-18 01:41:20 +0900

    first commit

---
+++ js/nav.js
@@ -0,0 +1,31 @@
+// get element
+const html = document.getElementById("html");
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+// test
+let hasJavaScript = true;
+
+// detect javascript
+if (hasJavaScript) {
+  html.classList.add("js");
+}
+
+// event binding
+menuBtn.addEventListener("click", toggleMenu, false);
+
+// turn on / off the menu
+function toggleMenu() {
+  // if the menu is active
+  if (menu.classList.contains("global-nav__menu--active")) {
+    // set aria-expanded of the button to false
+    this.setAttribute("aria-expanded", "false");
+    // open the menu
+    menu.classList.remove("global-nav__menu--active");
+  } else {
+    // set aria-expanded of the button to true
+    this.setAttribute("aria-expanded", "true");
+    // close the menu
+    menu.classList.add("global-nav__menu--active");
+  }
+}
